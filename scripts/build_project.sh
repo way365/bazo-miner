bcad9ec6e0ab391ba69916afe5d308946a03ff91
julius_willems <julius.willems@staff.cloud>
2020-03-10 11:22:09 +0100

    go-mod-package-manager changed execution order

--- scripts/build_project.sh
+++ scripts/build_project.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # This script builds the bazo-miner project.
-
-# First, we need to fix all the imports if we forked the project.
-./fix_imports.sh
-
-# Then we build the project
-echo Building the project...
-
-cd ..
-go get && go build
-
+echo Downloading dependencies...
+go get
 echo Done.
+
+# We need to fix all the imports if we forked the project.
+./scripts/fix_imports.sh
+
+echo Building the project...
+go build
+echo Done.
+
 echo You can now start the miner. To start it type \"bazo-miner start\" or follow the instructions in the README file.
-echo Enjoy...
+echo Enjoy!
